5b9904fffcb28ba740deedfe3c02dd1efc713cf5
Amel <akarsic1@etf.unsa.ba>
2018-10-25 15:14:11 +0200

    Formiranu su fiksnu i mobilni.

--- src/ba/unsa/etf/rpr/tutorijal03/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Main.java
@@ -3,6 +3,7 @@ package ba.unsa.etf.rpr.tutorijal03;
 public class Main {
 
     public static void main(String[] args) {
-
+        FiksniBroj a = new FiksniBroj(FiksniBroj.Grad.SARAJEVO,"123-456");
+        a.ispisi();
     }
 }
